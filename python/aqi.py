578073ef1b621bb1602fb8ee8f5e8a0daf2c222c
Forrest Bice <forrestbice@gmail.com>
2020-10-17 02:57:07 +0000

    Correct Formatting & Serial Dependency

--- python/aqi.py
+++ python/aqi.py
@@ -27,11 +27,11 @@ JSON_FILE_OUTDOOR = '/var/www/html/aqi_outdoor.json'
 MQTT_HOST = ''
 MQTT_TOPIC = '/weather/particulatematter'
 
-serial = serial.Serial()
-serial.port = "/dev/ttyUSB0"
-serial.baudrate = 9600
-serial.open()
-serial.flushInput()
+serial_indoor = serial.Serial()
+serial_indoor.port = "/dev/ttyUSB0"
+serial_indoor.baudrate = 9600
+serial_indoor.open()
+serial_indoor.flushInput()
 
 serial_outdoor = serial.Serial()
 serial_outdoor.port = "/dev/ttyUSB1"
@@ -39,17 +39,19 @@ serial_outdoor.baudrate = 9600
 serial_outdoor.open()
 serial_outdoor.flushInput()
 
+
 #byte, data = 0, ""
 
 def dump(d, prefix=''):
     print(prefix + ' '.join(x.encode('hex') for x in d))
 
+
 def construct_command(cmd, data=None):
     if data is None:
         data = []
     assert len(data) <= 12
-    data += [0,]*(12-len(data))
-    checksum = (sum(data)+cmd-2)%256
+    data += [0, ] * (12 - len(data))
+    checksum = (sum(data) + cmd - 2) % 256
     ret = "\xaa\xb4" + chr(cmd)
     ret += ''.join(chr(x) for x in data)
     ret += "\xff\xff" + chr(checksum) + "\xab"
@@ -58,18 +60,22 @@ def construct_command(cmd, data=None):
         dump(ret, '> ')
     return ret
 
+
 def process_data(d):
     r = struct.unpack('<HHxxBB', d[2:])
-    pm25 = r[0]/10.0
-    pm10 = r[1]/10.0
-    checksum = sum(ord(v) for v in d[2:8])%256
+    pm25 = r[0] / 10.0
+    pm10 = r[1] / 10.0
+    checksum = sum(ord(v) for v in d[2:8]) % 256
     return [pm25, pm10]
-    #print("PM 2.5: {} μg/m^3  PM 10: {} μg/m^3 CRC={}".format(pm25, pm10, "OK" if (checksum==r[2] and r[3]==0xab) else "NOK"))
+    # print("PM 2.5: {} μg/m^3  PM 10: {} μg/m^3 CRC={}".format(pm25, pm10, "OK" if (checksum==r[2] and r[3]==0xab) else "NOK"))
+
 
 def process_version(d):
     r = struct.unpack('<BBBHBB', d[3:])
-    checksum = sum(ord(v) for v in d[2:8])%256
-    print("Y: {}, M: {}, D: {}, ID: {}, CRC={}".format(r[0], r[1], r[2], hex(r[3]), "OK" if (checksum==r[4] and r[5]==0xab) else "NOK"))
+    checksum = sum(ord(v) for v in d[2:8]) % 256
+    print("Y: {}, M: {}, D: {}, ID: {}, CRC={}".format(r[0], r[1], r[2], hex(r[3]), "OK" if (
+                checksum == r[4] and r[5] == 0xab) else "NOK"))
+
 
 def read_response(ser):
     byte = 0
@@ -82,9 +88,11 @@ def read_response(ser):
         dump(d, '< ')
     return byte + d
 
+
 def cmd_set_mode(ser, mode=MODE_QUERY):
     ser.write(construct_command(CMD_MODE, [0x1, mode]))
     read_response(ser)
+
 
 def cmd_query_data(ser):
     ser.write(construct_command(CMD_QUERY_DATA))
@@ -94,26 +102,31 @@ def cmd_query_data(ser):
         vals = process_data(d)
     return vals
 
+
 def cmd_set_sleep(ser, sleep):
     mode = 0 if sleep else 1
     ser.write(construct_command(CMD_SLEEP, [0x1, mode]))
     ser.write(construct_command(CMD_SLEEP, [0x1, mode]))
     read_response(ser)
 
+
 def cmd_set_working_period(ser, period):
     ser.write(construct_command(CMD_WORKING_PERIOD, [0x1, period]))
     read_response(ser)
+
 
 def cmd_firmware_ver(ser):
     ser.write(construct_command(CMD_FIRMWARE))
     d = read_response(ser)
     process_version(d)
 
+
 def cmd_set_id(ser, id):
-    id_h = (id>>8) % 256
+    id_h = (id >> 8) % 256
     id_l = id % 256
     ser.write(construct_command(CMD_DEVICE_ID, [0] * 10 + [id_l, id_h]))
     read_response(ser)
+
 
 def pub_mqtt(jsonrow):
     cmd = ['mosquitto_pub', '-h', MQTT_HOST, '-t', MQTT_TOPIC, '-s']
@@ -121,8 +134,9 @@ def pub_mqtt(jsonrow):
     with subprocess.Popen(cmd, shell=False, bufsize=0, stdin=subprocess.PIPE).stdin as f:
         json.dump(jsonrow, f)
 
+
 def do_the_stuff(ser, json_file, sleep_duration):
-    #global data, sleep_time
+    # global data, sleep_time
     cmd_set_sleep(ser, 0)
     for t in range(15):
         values = cmd_query_data(ser)
@@ -152,16 +166,16 @@ def do_the_stuff(ser, json_file, sleep_duration):
 
 
 if __name__ == "__main__":
-    cmd_set_sleep(serial, 0)
+    cmd_set_sleep(serial_indoor, 0)
     cmd_set_sleep(serial_outdoor, 0)
-    cmd_firmware_ver(serial)
+    cmd_firmware_ver(serial_indoor)
     cmd_firmware_ver(serial_outdoor)
-    cmd_set_working_period(serial, PERIOD_CONTINUOUS)
+    cmd_set_working_period(serial_indoor, PERIOD_CONTINUOUS)
     cmd_set_working_period(serial_outdoor, PERIOD_CONTINUOUS)
-    cmd_set_mode(serial, MODE_QUERY)
+    cmd_set_mode(serial_indoor, MODE_QUERY)
     cmd_set_mode(serial_outdoor, MODE_QUERY)
     while True:
         sleep_time = 60
-        do_the_stuff(serial, JSON_FILE, sleep_time)
-        #do_the_stuff(serial_outdoor, JSON_FILE_OUTDOOR, sleep_time)
+        do_the_stuff(serial_indoor, JSON_FILE, sleep_time)
+        # do_the_stuff(serial_outdoor, JSON_FILE_OUTDOOR, sleep_time)
         time.sleep(sleep_time)
